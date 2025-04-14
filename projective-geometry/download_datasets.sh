54e34e520f9a062f6b211a3a19f6527d08ed01f7
Yi Tjun Yam <yitjunyam@hotmail.com>
2025-04-14 13:31:09 +0200

    modified download dataset

--- projective-geometry/download_datasets.sh
+++ projective-geometry/download_datasets.sh
@@ -2,18 +2,23 @@ echo "Downloading paper datasets"
 cd dataset
 
 ZIPS=(
-    https://huggingface.co/datasets/amitabh3/Projective-Geometry/resolve/main/Deepfloyd_Indoor.zip
-    https://huggingface.co/datasets/amitabh3/Projective-Geometry/resolve/main/Deepfloyd_Outdoor.zip
-    https://huggingface.co/datasets/amitabh3/Projective-Geometry/resolve/main/FLUX_Indoor.zip
-    https://huggingface.co/datasets/amitabh3/Projective-Geometry/resolve/main/Kandinsky_Indoor.zip
-    https://huggingface.co/datasets/amitabh3/Projective-Geometry/resolve/main/Kandinsky_Outdoor.zip
-    https://huggingface.co/datasets/amitabh3/Projective-Geometry/resolve/main/PixArt_Indoor.zip
-    https://huggingface.co/datasets/amitabh3/Projective-Geometry/resolve/main/PixArt_Outdoor.zip
+    https://huggingface.co/datasets/amitabh3/Projective-Geometry/resolve/main/Recent_Deepfloyd_Indoor.zip
+    https://huggingface.co/datasets/amitabh3/Projective-Geometry/resolve/main/Recent_Deepfloyd_Outdoor.zip
+    https://huggingface.co/datasets/amitabh3/Projective-Geometry/resolve/main/Recent_Kandinsky_Indoor.zip
+    https://huggingface.co/datasets/amitabh3/Projective-Geometry/resolve/main/Recent_Kandinsky_Outdoor.zip
+    https://huggingface.co/datasets/amitabh3/Projective-Geometry/resolve/main/Recent_Pixart_Indoor.zip
+    https://huggingface.co/datasets/amitabh3/Projective-Geometry/resolve/main/Recent_Pixart_Outdoor.zip
+    https://huggingface.co/datasets/amitabh3/Projective-Geometry/resolve/main/Recent_SDXL_Indoor.zip
+    https://huggingface.co/datasets/amitabh3/Projective-Geometry/resolve/main/Recent_SDXL_Outdoor.zip
 
 )
-for zip in $ZIPS; do
+for zip in "${ZIPS[@]}"; do
     echo "Downloading $zip"
     wget --no-verbose "$zip"
+done
+
+for zip in *.zip; do
+    echo "Processing $zip"
     unzip "$zip"
     rm "$zip"
 done
