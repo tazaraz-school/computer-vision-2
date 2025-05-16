5c526da0c7b3b7843265189d03ffa19129e874b0
tazaraz <taz@t32.nl>
2025-05-16 12:50:48 +0200

    Added weights

--- DeepLSD/quickstart_install.sh
+++ DeepLSD/quickstart_install.sh
@@ -1,4 +1,8 @@
-pip install cmake==3.18
+pip install cmake==3.18  # Specific version of cmake
 pip install scikit-build
 pip install -r requirements.txt  # Install the requirements
 pip install -e .  # Install DeepLSD
+
+mkdir weights
+wget https://cvg-data.inf.ethz.ch/DeepLSD/deeplsd_wireframe.tar -O weights/deeplsd_wireframe.tar
+wget https://cvg-data.inf.ethz.ch/DeepLSD/deeplsd_md.tar -O weights/deeplsd_md.tar
