7dab455dc2055fd58bc405dd961eae25c9a3d174
tazaraz <taz@t32.nl>
2025-05-16 14:07:18 +0200

    Whatever

--- DeepLSD/quickstart_install.sh
+++ DeepLSD/quickstart_install.sh
@@ -1,4 +1,6 @@
 pip install cmake==3.18  # Specific version of cmake
+cd third_party/pytlsd/pybind11 && pip install .  # Install pybind11
+
 pip install scikit-build
 pip install -r requirements.txt  # Install the requirements
 pip install -e .  # Install DeepLSD
